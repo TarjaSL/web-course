6c9304e06e8d635bf380943e468ce410bb0e7f0f
TarjaSL <svetozar-vk@mail.ru>
2016-03-10 16:54:33 +0300

    From office 6

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,18 +1,13 @@
 from django.conf.urls import include, url
 from qa.views import test
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$', test, name='index'),
-    url(r'^login/', test, name='login'),
-    url(r'^signup/', est, name='signup'),
-    url(r'^question/(?P<id>[0-9]+)/$', test, name='question'),
-    url(r'^ask/', test, name='ask'),
-    url(r'^popular/', test, name='popular'),
-    url(r'^new/', test, name='new'),
-    
-]
-
+urlpatterns = patterns('qa.views',
+   url(r'^$', 'test', name='index'),
+   url(r'^login/', 'test', name='login'),
+   url(r'^signup/', 'test', name='signup'),
+   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),
+   url(r'^ask/', 'test', name='ask'),
+   url(r'^popular/', 'test', name='popular'),   
+   url(r'^new/', 'test', name='new'),  
+)
 
