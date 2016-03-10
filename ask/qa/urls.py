65ec48962ecfb6342c8502521a09fc1744897cb9
TarjaSL <svetozar-vk@mail.ru>
2016-03-10 14:48:29 +0300

    From office 3

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -6,13 +6,13 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'ask.views.test', name='index'),
-    url(r'^login/', 'ask.views.test', name='login'),
-    url(r'^signup/', 'ask.views.test', name='signup'),
-    url(r'^question/(?P<id>[0-9]+)/$', 'ask.views.test', name='question'),
-    url(r'^ask/', 'ask.views.test', name='ask'),
-    url(r'^popular/', 'ask.views.test', name='popular'),
-    url(r'^new/', 'ask.views.test', name='new'),
+    url(r'^$', views.test, name='index'),
+    url(r'^login/', views.test, name='login'),
+    url(r'^signup/', views.test, name='signup'),
+    url(r'^question/(?P<id>[0-9]+)/$', views.test, name='question'),
+    url(r'^ask/', views.test, name='ask'),
+    url(r'^popular/', views.test, name='popular'),
+    url(r'^new/', views.test, name='new'),
     
 ]
 
