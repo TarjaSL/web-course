d0aa6c60a73cc8181bc21330d4cd45333b82080e
TarjaSL <svetozar-vk@mail.ru>
2016-03-10 14:55:52 +0300

    From office 4

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
 admin.autodiscover()
-from ask.views import test
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     url(r'^', include('qa.urls')),
     url(r'^admin/', include('admin.site.urls')),
-)
+]
