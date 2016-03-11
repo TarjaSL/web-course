ff3c5d5559edc642267a41af6175e484efce0ae8
TarjaSL <svetozar-vk@mail.ru>
2016-03-11 12:51:18 +0300

    New day!6

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-url(r'^', include('ask.urls')),
-url(r'^admin/', include('admin.site.urls')),
+    url(r'^$', 'qa.views.test'),
+    url(r'^login/.*$', 'qa.views.test'),
+    url(r'^signup/.*$', 'qa.views.test'),
+    url(r'^questions/\d+/$', 'qa.views.test'),
+    url(r'^ask/.*$', 'qa.views.test'),
+    url(r'^popular/.*$', 'qa.views.test'),
+    url(r'^new/.*$', 'qa.views.test')
 ]
--- ask/qa/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import include, url, patterns
-
-from qa.views import test
-urlpatterns = patterns('qa.views',
-url(r'^$', test),
-)
